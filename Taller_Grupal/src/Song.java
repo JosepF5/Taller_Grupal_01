c53bb75f840bcfe292d325324cee575355ba0138
Josep Palomino <jfpalomino.22@gmail.com>
2022-02-21 10:29:11 -0500

    Cambio Song

--- Taller_Grupal/src/Song.java
+++ Taller_Grupal/src/Song.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -34,3 +35,43 @@ public class Song {
     
  
 }
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Date;
+
+/**
+ *
+ * @author USUARIO si
+ */
+public class Song {
+    public String title;
+    public int ID;
+    public Date date;
+    public String duration;
+    public String genre;
+    public String cover;
+    public String description;
+
+    public Song(String title, int ID, Date date, String duration, String genre, String cover, String description) {
+        this.title = title;
+        this.ID = ID;
+        this.date = date;
+        this.duration = duration;
+        this.genre = genre;
+        this.cover = cover;
+        this.description = description;
+    }
+
+
+    
+
+
+    
+ 
+}
+>>>>>>> Stashed changes
